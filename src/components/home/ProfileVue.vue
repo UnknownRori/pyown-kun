e9c1360f6b2014e861628475777e59f3172758a1
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-03 02:10:25 +0700

    🛠️ Minor styling changes

--- src/components/home/ProfileVue.vue
+++ src/components/home/ProfileVue.vue
@@ -3,7 +3,7 @@ import Profile from "@/assets/image/profile/profile.webp";
 </script>
 
 <template>
-    <figure>
+    <figure class='flex flex-col gap-4'>
         <img class='w-52 shadow-md rounded-full border-2 border-gray-400' :src='Profile' alt="Profile Image">
         <figcaption class='text-center text-2xl'>{{ $firebase.name }}</figcaption>
     </figure>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,15 @@ import LayoutVue from '@/layouts/LayoutVue.vue';
 </script>
 
 <template>
-  <LayoutVue>
-    <section>
-      <ProfileVue />
-    </section>
+  <LayoutVue class=' flex flex-col justify-center items-center min-h-screen m-2'>
+    <div class='sm:w-full md:w-3/4 rounded-2xl py-6 shadow-lg flex flex-col justify-center items-center gap-12'>
+      <section>
+        <ProfileVue />
+      </section>
 
-    <section>
-      <SocialListVue />
-    </section>
+      <section class='w-full flex flex-col'>
+        <SocialListVue />
+      </section>
+    </div>
   </LayoutVue>
 </template>
