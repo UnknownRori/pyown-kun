d9fb0309dadcf3aafd32f757a746842343bc6239
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-02 08:13:44 +0700

    🛠️ Rename file

--- src/components/social/SocialMediaListVue.vue
+++ src/components/social/SocialMediaListVue.vue
@@ -1,6 +1,13 @@
 <script setup lang='ts'>
+import SocialLists from '@/data/SocialLists';
+import SocialItem from '@/components/social/SocialMediaItemVue.vue';
+
+
 </script>
 
 <template>
-    <h2>Hello, SocialMediaList</h2>
+    <ul class='flex flex-col gap-2'>
+        <SocialItem v-for='(SocialList, index) in SocialLists' :key='index' :icon='SocialList.icon' :link='SocialList.link'
+            :name='SocialList.name' />
+    </ul>
 </template>
--- src/components/social/SocialMediaVue.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang='ts'>
-</script>
-
-<template>
-    <h2>Hello, SocialMedia</h2>
-</template>
