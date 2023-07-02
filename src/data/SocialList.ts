34514788e006e64dcad5c638fe7b0d588317f681
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-02 08:09:32 +0700

    🛠️ Rename from `SocialList.ts` to `SocialLists.ts`

--- src/data/SocialList.ts
+++
@@ -1,9 +0,0 @@
-import type SocialList from "@/types/SocialList";
-
-// TODO : Download and add icons here
-const socialList = [
-    { name: "Instagram", link: "https://instagram.com/pyown_kun?igshid=MzNlNGNkZWQ4Mg==", icon: "" },
-    { name: "Twitter", link: "https://twitter.com/Pyown_kun?t=1CZlzhopMVS7KbBec6eIUw&s=08", icon: "" },
-] as Array<SocialList>;
-
-export default socialList;
---
+++ src/data/SocialLists.ts
@@ -0,0 +1,12 @@
+import Twitter from '@/assets/image/social/twitter.svg';
+import Instagram from '@/assets/image/social/instagram.svg';
+
+import type SocialList from "@/types/SocialList";
+
+// TODO : Download and add icons here
+const socialLists = [
+    { name: "Instagram", link: "https://instagram.com/pyown_kun?igshid=MzNlNGNkZWQ4Mg==", icon: Instagram },
+    { name: "Twitter", link: "https://twitter.com/Pyown_kun?t=1CZlzhopMVS7KbBec6eIUw&s=08", icon: Twitter },
+] as Array<SocialList>;
+
+export default socialLists;
