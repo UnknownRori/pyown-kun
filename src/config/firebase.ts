90baffa71595f0a6c52a6ed6a92fb47e3065d2bb
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-03 07:14:52 +0700

    🛠️ Fix some github action issue

---
+++ src/config/firebase.ts
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+
+import type { FirebaseOptions } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCZhXongTBWm8pAvs9lIVX_8TCASHFS5wo",
+    authDomain: "pyown-kun.firebaseapp.com",
+    projectId: "pyown-kun",
+    storageBucket: "pyown-kun.appspot.com",
+    messagingSenderId: "854337614343",
+    appId: "1:854337614343:web:c673200de6b2b95ac4f0a2",
+    measurementId: "G-T43QPGPF17",
+} as FirebaseOptions;
+
+export { firebaseConfig };
