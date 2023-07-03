9f5dd95ef13f106064d2481cee499182a69532c9
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-03 18:08:13 +0700

    📦 Added more social list

--- src/data/SocialLists.ts
+++ src/data/SocialLists.ts
@@ -1,12 +1,16 @@
 import Twitter from '@/assets/image/social/twitter.svg';
 import Instagram from '@/assets/image/social/instagram.svg';
+import Pixiv from '@/assets/image/social/pixiv.svg';
+import Facebook from '@/assets/image/social/facebook.svg';
 
 import type SocialList from "@/types/SocialList";
 
 // TODO : Download and add icons here
 const socialLists = [
-    { name: "Instagram", link: "https://instagram.com/pyown_kun?igshid=MzNlNGNkZWQ4Mg==", icon: Instagram },
+    { name: "Pixiv", link: "https://www.pixiv.net/users/94250146", icon: Pixiv },
     { name: "Twitter", link: "https://twitter.com/Pyown_kun?t=1CZlzhopMVS7KbBec6eIUw&s=08", icon: Twitter },
+    { name: "Facebook", link: "https://www.facebook.com/profile.php?id=100092453631515&mibextid=ZbWKwL", icon: Facebook },
+    { name: "Instagram", link: "https://instagram.com/pyown_kun?igshid=MzNlNGNkZWQ4Mg==", icon: Instagram },
 ] as Array<SocialList>;
 
 export default socialLists;
